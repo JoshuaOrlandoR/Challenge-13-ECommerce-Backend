69bd15787036e2b677321363de5f032969eacf7f
JoshuaOrlandoR <joshuaorlando.r@gmail.com>
2023-01-15 15:45:39 -0500

    error on deletes, possible syntax error fixed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -88,7 +88,7 @@ router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
   Category.destroy({
     where: {
-      id:req.params.id
+      id: req.params.id
     }
   })
   .then(categoryInfo => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -74,7 +74,7 @@ router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
   Tag.destory({
     where: {
-      id:req.params.id
+      id: req.params.id
     }
   })
   .then(tagInfo => {
